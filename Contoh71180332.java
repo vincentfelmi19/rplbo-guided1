724189c529a77bebbeff4eb8e44c5c799bd8b4ca
vincentfelmi19 <vincentsius.felmi@ti.ukdw.ac.id>
2022-08-31 15:28:14 +0700

    fix selesai

---
+++ Contoh71180332.java
@@ -0,0 +1,14 @@
+public class Contoh71180332{
+    public static void main(String args[]){
+        int x = 10;
+        int y = 25;
+        int z = x+y;
+
+        System.out.println("Hasil Penjumlahan"+" dari x+y = "+z);
+        System.out.println("Hasil akar kuadrat "+"dari z = " + Math.sqrt(z));
+
+        String namadepan = "Antonius";
+        System.out.println("Nama depan saya adalah = "+ namadepan);
+        System.out.println("Nama lengkap saya "+"adalah = "+namadepan+" Rachmat");
+    }
+}
